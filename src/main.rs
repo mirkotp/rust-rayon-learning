b9cad8095ae199cc578f8fc765164a198874123a
Mirko <mirkox90@gmail.com>
2023-04-28 19:44:17 +0200

    testing zip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rayon::prelude::{IntoParallelIterator, IndexedParallelIterator, ParallelIterator};
+
 fn main() {
-    println!("Look at ./benches");
+    println!("Also look at ./benches");
+    test_zip();
+}
+
+fn test_zip() {
+    let a = 1..1000;
+    let b = 1001..2000;
+
+    a.into_par_iter().zip(b.into_par_iter()).for_each(|(e1, e2)| {
+        println!("{} - {}", e1, e2);
+    });
+
+    // Items are paired correctly, sample output
+    // 250 - 1250
+    // 251 - 1251
+    // 875 - 1875
+    // 876 - 1876
+    // 750 - 1750
+    // 751 - 1751
+    // 752 - 1752
+    // ...
 }
